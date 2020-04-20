708d3429872b2a579bc9049bdf7ca16871b47674
ParkInsoo <62046885+ParkInsoo@users.noreply.github.com>
2020-04-20 23:39:03 +0900

    Create insoo.cpp
    abstract class

---
+++ Abstract Classes - Polymorphism/insoo.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cassert>
+using namespace std;
+
+struct Node{
+   Node* next;
+   Node* prev;
+   int value;
+   int key;
+   Node(Node* p, Node* n, int k, int val):prev(p),next(n),key(k),value(val){};
+   Node(int k, int val):prev(NULL),next(NULL),key(k),value(val){};
+};
+
+class Cache{
+   
+   protected: 
+   map<int,Node*> mp; //map the key to the node in the linked list
+   int cp;  //capacity
+   Node* tail; // double linked list tail pointer
+   Node* head; // double linked list head pointer
+   virtual void set(int, int) = 0; //set function
+   virtual int get(int) = 0; //get function
+
+};
+
+class LRUCache : public Cache
+{
+public:
+    LRUCache(int capacity) : m_capacity(capacity), m_size(0)
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void set(int key, int value)
+    {
+        Node *pNode = NULL;
+        Node *node = mp[key];
+        if (NULL == node)
+        {
+            pNode = new Node(key, value);
+            //mp.insert(std::pair<int,Node*>(key, pNode));
+            mp[key] = pNode;
+            m_size++;
+        }
+        else
+        {
+            node->value = value;
+            pNode = node;
+        }
+
+        Node *pTail = this->tail;
+        this->tail = pNode;
+        pNode->prev = pTail;
+        if (NULL == this->head)
+            this->head = pNode;
+
+        if (m_size > m_capacity)
+        {
+            Node *pHead = this->head;
+            this->head = pHead->next;
+
+            mp.erase(pHead->key);
+            delete pHead;
+            m_size--;
+        }
+    }
+
+    int get(int key)
+    {
+        Node *node = mp[key];
+        if (NULL == node)
+            return -1;
+        return node->value;
+    }
+private:
+    int m_capacity;
+    int m_size;
+};
+
+int main() {
+   int n, capacity,i;
+   cin >> n >> capacity;
+   LRUCache l(capacity);
+   for(i=0;i<n;i++) {
+      string command;
+      cin >> command;
+      if(command == "get") {
+         int key;
+         cin >> key;
+         cout << l.get(key) << endl;
+      } 
+      else if(command == "set") {
+         int key, value;
+         cin >> key >> value;
+         l.set(key,value);
+      }
+   }
+   return 0;
+}
